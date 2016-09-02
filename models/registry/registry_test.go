d6f1cbbf22cce3af681f69615a14a6ec1ef3499a
Stefan Naglee <stefannaglee@gmail.com>
2016-09-02 08:02:45 -0400

    Added startup and shutdown of registry container for testing

--- models/registry/registry_test.go
+++ models/registry/registry_test.go
@@ -1,10 +1,22 @@
 package registry
 
 import (
+	"os"
 	"testing"
+	"time"
 
+	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
 	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/net/context"
 )
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
 
 // TestAddRegistry tests the AddRegistry function
 func TestParseRegistry(t *testing.T) {
@@ -31,5 +43,35 @@ func TestParseRegistry(t *testing.T) {
 	Convey("When we pass an invalid RegistryURI we should get back the registry type with errors", t, func() {
 		So(err, ShouldNotBeNil)
 	})
+}
+
+func setup() {
+
+	// Connect to the local docker client
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = cli.ContainerStart(context.Background(), "registry", types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+}
+
+func shutdown() {
+
+	// Connect to the local docker client
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
+	if err != nil {
+		panic(err)
+	}
+
+	timeout, err := time.ParseDuration("10s")
+
+	if err = cli.ContainerStop(context.Background(), "registry", &timeout); err != nil {
+		panic(err)
+	}
 
 }
